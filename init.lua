542a87b69cd195413a7531309b4984fbdf513472
OKABE Gota <go.sirasagi62@gmail.com>
2023-07-13 02:51:16 +0900

    cursorlineが正常になった

--- init.lua
+++ init.lua
@@ -42,24 +42,4 @@ require('jetpack.packer').add {
 
 -- setting for colorscheme
 vim.opt.cursorline = true
-require("tokyonight").setup({
-  style = "storm",
-  on_highlights = function(hl, c)
-    hl.LineNr = {
-      fg = c.yellow
-    }
-  end,
-})
 vim.cmd[[colorscheme tokyonight]]
--- set line number color to yellow.
--- [[
-vim.api.nvim_exec('highlight LineNr ctermfg = 228',false)
--- Sets colors to line numbers Above, Current and Below  in this order
-function LineNumberColors()
-    vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#51B3EC', bold=true })
-    --vim.api.nvim_set_hl(0, 'LineNr', { fg='white', bold=true })
-    vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='#FB508F', bold=true })
-end
-
-LineNumberColors()
-]]--
